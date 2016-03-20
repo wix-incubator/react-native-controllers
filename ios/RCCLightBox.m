592c12bb60c5062df51c61e946b0816bab02cf27
Adam Wilson <adam@codegarden.co.uk>
2016-03-20 20:57:16 +0000

    rename sharedIntance to sharedInstance

--- ios/RCCLightBox.m
+++ ios/RCCLightBox.m
@@ -47,7 +47,7 @@ const NSInteger kLightBoxTag = 0x101010;
             }
         }
         
-        self.reactView = [[RCTRootView alloc] initWithBridge:[[RCCManager sharedIntance] getBridge] moduleName:self.params[@"component"] initialProperties:nil];
+        self.reactView = [[RCTRootView alloc] initWithBridge:[[RCCManager sharedInstance] getBridge] moduleName:self.params[@"component"] initialProperties:nil];
         self.reactView.sizeFlexibility = RCTRootViewSizeFlexibilityWidthAndHeight;
         self.reactView.center = self.center;
         [self addSubview:self.reactView];
