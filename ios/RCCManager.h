ff18bf1d4769b55aef8f9042c20819d54a4c9f6f
Benoit Nolens <benoit@truestory.io>
2016-06-16 22:17:49 +0200

    Pass launchOptions to make Linking work

--- ios/RCCManager.h
+++ ios/RCCManager.h
@@ -7,7 +7,7 @@
 + (instancetype)sharedInstance;
 + (instancetype)sharedIntance;
 
--(void)initBridgeWithBundleURL:(NSURL *)bundleURL;
+-(void)initBridgeWithBundleURL:(NSURL *)bundleURL launchOptions:(NSDictionary *)launchOptions;
 -(RCTBridge*)getBridge;
 
 -(void)registerController:(UIViewController*)controller componentId:(NSString*)componentId componentType:(NSString*)componentType;
